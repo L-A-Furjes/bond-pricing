4ed9118005c74f233f5a281db1866572f2a5b723
Lucas-Aleksander Furjes <lucas-aleksander.furjes@edu.devinci.fr>
2025-07-11 03:36:43 +0200

    Exo 2: add price_dirty and accrued_interest functions, partial progress toward full clean price implementation

--- src/bond_price.py
+++ src/bond_price.py
@@ -2,8 +2,27 @@ from bond_cashflows import cashflows
 from datetime import datetime
 import numpy as np
 import pandas as pd
+from typing import Union
 
-def accrued_interet(cf_df,issue_date:str| pd.Timestamp, settle_date:str| pd.Timestamp,coupon_rate:float, frequency:int, nominal: float) -> float:
+def price_dirty(cf_df, settle_date: Union [str,pd.Timestamp],yld: float) -> float:
+
+    settle_date = pd.to_datetime(settle_date)
+    
+    future_cf = cf_df[cf_df['Dates'] > settle_date]
+    cashflows = future_cf['Cashflows']
+    dates = future_cf['Dates']
+    t_list = [(date - settle_date).days/365 for date in dates]
+
+
+    dirty_price = sum(c/((1+yld)**t) for c,t in zip(cashflows,t_list))
+    
+    return dirty_price
+
+
+
+
+
+def accrued_interest(cf_df,issue_date: Union [str, pd.Timestamp], settle_date:Union [str, pd.Timestamp],coupon_rate:float, frequency:int, nominal: float) -> float:
     
     df_Dates = cf_df['Dates']
     settle_date = pd.to_datetime(settle_date)
@@ -17,10 +36,17 @@ def accrued_interet(cf_df,issue_date:str| pd.Timestamp, settle_date:str| pd.Time
     ai = coupon * ((settle_date - last_coupon).days/(next_coupon-last_coupon).days)
     return ai
 
+
+
+def price_clean(dirty: float, accrued: float) -> float:
+    return dirty - accrued
+
 if __name__ == '__main__':
     df = cashflows('2022-02-20','2025-03-25',0.05,2,nominal=1000)
-    af = accrued_interet(df,'2022-02-20','2022-03-20',0.05,2,nominal=1000)
-    print(af)
+    df_dirty = price_dirty(df,'2023-03-20',0.05)
+    df_ai = accrued_interest(df, '2022-02-20', '2023-03-20', 0.05, 2, nominal=1000)
+    df_clean = price_clean(df_dirty,df_ai)
+    print(df_clean)
 
 
 
